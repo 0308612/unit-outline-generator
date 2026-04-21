f383daa9518fe05e7456ec4d29398a3872aa3fc9
Sebastian Schwenke <0308612@schoolsnet.act.edu.au>
2026-04-21 10:17:06 +1000

    following django tutorial for file uploads

--- InClassDjango/MyApp/views.py
+++ InClassDjango/MyApp/views.py
@@ -1,15 +1,15 @@
-
-
 from urllib import request, response
 from xml.dom.minidom import Document
 from .models import teacher
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
 from pypdf import PdfReader
 
 from .models import teacher
 from .forms import InputForm
 import io
+import shutil
 
 # Create your views here.
 def index(request):
@@ -22,18 +22,25 @@ def input_view(request):
     if request.method == "POST":
 
         form = InputForm(request.POST)
-        pdf_file = request.FILES['pdf_file']
-        pdf_content = pdf_file.read()
-        pdf_buffer = io.BytesIO(pdf_content)
-        form.cleaned_data['PDF_File']
-        reader = PdfReader(pdf_buffer)
-        print(len(reader.pages))
-        page = reader.pages[0]
-        text = page.extract_text()
-        print(text)
+        form = InputForm(request.POST, request.FILES)
+        #pdf_file = request.FILES.get('pdf_file')
+        ##pdf_file = InputForm.cleaned_data['pdf_file'] #cleaned data doesn't exist
+        #fs = FileSystemStorage
+        #filename = fs.save(pdf_file.name, pdf_file)
+        #with open(filename, "rb") as fh:
+        #    memoryFile = io.BytesIO(fh.read())
+        #if memoryFile:
+        #    pdf_content = memoryFile.read()
+        #    pdf_buffer = io.BytesIO(pdf_content)
+        #    form.cleaned_data['PDF_File']
+        #    reader = PdfReader(pdf_buffer)
+        #    print(len(reader.pages))
+        #    page = reader.pages[0]
+        #    text = page.extract_text()
+        #    print(text)
 
         if form.is_valid():
-
+            handle_uploaded_file(request.FILES["file"])
             form.save()
             return redirect('index')
 
@@ -47,6 +54,10 @@ def input_view(request):
     return render(request, "MyApp/input.html", {"form": form})
 
 
+def handle_uploaded_file(f):
+    with open("some/file/name.pdf", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 
 #def prosses_pdf_data(request, doc_id):
 #    document = teacher.objects.get(id = doc_id)
